1490273066d635101b0884ae599bb193e1a6742b
Stephan Duncan <mac27@Mac27s-iMac.local>
2015-11-03 12:36:55 -0700

    added files for supercool

--- Super Cool/ViewController.swift
+++ Super Cool/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   
+    @IBOutlet var blueBalloon: UIImageView!
+    @IBOutlet var redBalloon: UIImageView!
+    @IBOutlet var hideBlue: UIButton!
+    @IBOutlet var hideRed: UIButton!
+
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +28,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func makeBlueGoAway(sender: AnyObject) {
+        blueBalloon.hidden = true
+    }
 
+    @IBAction func makeRedGoAway(sender: AnyObject) {
+        redBalloon.hidden = true
+    }
+    
 }
 
